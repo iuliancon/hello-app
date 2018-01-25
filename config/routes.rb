46bf90e936dbf0ee7ea140748b84d54cef68216b
Iulian Constantin <iulian.constant@gmail.com>
2018-01-25 08:14:40 +0000

    Adds new controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  # iuco - by rails
+  #get 'hello/index'
+  # iuco if the browser asks for /hello, run controller class 'hello' with
+  #  method 'index'
+  get 'hello' => 'hello#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # iuco - change root action to be application controller -> hello method
   #
-  root "application#hello"
+  # iuco - another method to call a controller class - 'application' with 
+  # a method of 'hello'
+  #
+  #root "application#hello"
 end
