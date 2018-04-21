4c279c29509cd306711b81993f57534b6969dc20
Iulian Constantin <iulian.constant@gmail.com>
2018-04-21 13:16:01 +0000

    Adds more messages to hello controller.

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -2,6 +2,7 @@ class HelloController < ApplicationController
   def index
     @message="Hello!"
     @count=10
-    @bonus="This hello message came from the controller and also from *** Iulian ***"
+    @bonus="This hello message came from the controller and also from  *** Iulian ***"
+    @msg="This hello message came from the controller and also from  +++ Jules +++"
   end
 end
